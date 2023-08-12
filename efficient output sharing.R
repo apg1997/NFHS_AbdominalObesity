3ed13299aa0662eea4e0ef3d3de853a2ac591766
Tushar Garg <gargtush@gmail.com>
2023-08-12 18:49:05 +0530

    solution for sharing output using smaller Rdata files

---
+++ efficient output sharing.R
@@ -0,0 +1,54 @@
+suppressMessages(if (!require("pacman")) install.packages("pacman"))
+pacman::p_load(tidyverse,rio,here,gtsummary)
+
+# Currently, the Rdata output for analysis of complete women (9GB) and men (2GB) data are too large to handle efficiently.
+
+# Issues are 1. saving original datasets as well: women_original and men_original, 2. large table objects
+# saving only essential objects now
+# gtsummary::tbl_buther will reduce the size of tbl_x objects
+
+########
+# Men
+########
+
+load(here("Output","men_output.Rdata"))
+
+save(list = setdiff(ls(pattern = "^men_"), "men_original"), file = here("Output", "men_output_smaller.Rdata"))
+
+# Identify tbl_x objects
+objs_to_butcher <- ls()[sapply(ls(), function(obj_name) {
+  obj <- get(obj_name)
+  any(c("tbl_regression", "tbl_uvregression", "tbl_svysummary") %in% class(obj))
+})]
+
+# Apply tbl_butcher to those objects and assign back to the same name
+for(obj_name in objs_to_butcher) {
+  assign(obj_name, tbl_butcher(get(obj_name)))
+}
+
+save(list = setdiff(ls(pattern = "^men_"), "men_original"), file = here("Output", "men_output_smallest.Rdata"))
+
+#######################################
+
+rm(list = ls())
+
+########
+# Women
+########
+
+load(here("Output","women_output.Rdata"))
+
+save(list = setdiff(ls(pattern = "^women_"), "women_original"), file = here("Output", "women_output_smaller.Rdata"))
+
+# Identify tbl_x objects
+objs_to_butcher <- ls()[sapply(ls(), function(obj_name) {
+  obj <- get(obj_name)
+  any(c("tbl_regression", "tbl_uvregression", "tbl_svysummary") %in% class(obj))
+})]
+
+# Apply tbl_butcher to those objects and assign back to the same name
+for(obj_name in objs_to_butcher) {
+  assign(obj_name, tbl_butcher(get(obj_name)))
+}
+
+save(list = setdiff(ls(pattern = "^women_"), "women_original"), file = here("Output", "women_output_smallest.Rdata"))
